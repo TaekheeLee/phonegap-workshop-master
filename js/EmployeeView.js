7ab1fafcd6247751d082a46cdafed1d77779e6c2
TaekheeLee <thlee90@naver.com>
2013-05-23 14:55:48 +0900

    Contacts API

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -8,6 +8,7 @@ var EmployeeView = function(employee){
     this.render = function() {
         this.el.html(EmployeeView.template(employee));
         this.el.on('click', '.add-location-btn', this.addLocation);
+        this.el.on('click', '.add-contact-btn', this.addToContacts);
         
         return this;
     };
@@ -24,6 +25,25 @@ var EmployeeView = function(employee){
             });
         return false;
     };
+    
+     
+    this.addToContacts = function(event) {
+        event.preventDefault();
+        console.log('addToContacts');
+        if (!navigator.contacts) {
+            app.showAlert("Contacts API not supported", "Error");
+            return;
+        }
+        var contact = navigator.contacts.create();
+        contact.name = {givenName: employee.firstName, familyName: employee.lastName};
+        var phoneNumbers = [];
+        phoneNumbers[0] = new ContactField('work', employee.officePhone, false);
+        phoneNumbers[1] = new ContactField('mobile', employee.cellPhone, true); // preferred number
+        contact.phoneNumbers = phoneNumbers;
+        contact.save();
+        return false;
+    };
+     
 
 }
 
